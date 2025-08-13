cafeef7e4eb43f60d38e4eadb4650322c8d55897
tehziibqueen25-svg <tehziibqueen25@gmail.com>
2025-08-13 20:38:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~uFVlnAhZ#3rY5FaNWKsD9yKQyTaXwNYfJAD9KvchrXJsLNNDhk-U",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
